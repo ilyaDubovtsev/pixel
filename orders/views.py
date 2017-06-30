14d78d376015b170a71a5e7536427117fca16649
ilya <dubovtsev98@gmail.com>
2017-06-30 20:06:52 +0400

    +front-end was added (u must fix it)

--- orders/views.py
+++ orders/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render
 from django.utils import timezone
 from .models import Order
 
+
 # Create your views here.
 
 def order_list(request):
     orders = Order.objects.filter(sended_date__lte=timezone.now()).order_by('sended_date')
-    return render(request, 'orders/order_list.html', {'orders' : orders})
+    return render(request, 'orders/order.html', {'orders' : orders})
